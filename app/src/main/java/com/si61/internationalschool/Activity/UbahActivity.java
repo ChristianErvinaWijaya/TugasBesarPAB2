17ab696fd51000fc62c55ef3aed4bcbde86d3ed3
IndraKosasih <100412660+IndraKosasih@users.noreply.github.com>
2023-06-21 22:13:20 +0700

    Ubah dan Delete Activity

--- app/src/main/java/com/si61/internationalschool/Activity/UbahActivity.java
+++ app/src/main/java/com/si61/internationalschool/Activity/UbahActivity.java
@@ -2,15 +2,94 @@ package com.si61.internationalschool.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.si61.internationalschool.API.APIRequestData;
+import com.si61.internationalschool.API.RetroServer;
+import com.si61.internationalschool.Model.ModelResponse;
 import com.si61.internationalschool.R;
 
-public class UbahActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+public class UbahActivity extends AppCompatActivity {
+    private EditText etNama, etAlamat, etNoTelepon, etFasilitas;
+    private Button btnSimpan;
+    private String nama, alamat, noTelepon, fasilitas;
+    private String yId, yNama, yAlamat, yNotelp, yFasilitas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubah);
+
+        etNama = findViewById(R.id.et_nama);
+        etAlamat = findViewById(R.id.et_alamat);
+        etNoTelepon = findViewById(R.id.et_noTelepon);
+        etFasilitas = findViewById(R.id.et_fasilitas);
+        btnSimpan = findViewById(R.id.btn_simpan);
+
+        Intent tangkap = getIntent();
+        yId = tangkap.getStringExtra("xId");
+        yNama = tangkap.getStringExtra("xNama");
+        yAlamat = tangkap.getStringExtra("xAlamat");
+        yNotelp = tangkap.getStringExtra("xNoTelp");
+        yFasilitas = tangkap.getStringExtra("xFasilitas");
+
+        etNama.setText(yNama);
+        etAlamat.setText(yAlamat);
+        etNoTelepon.setText(yNotelp);
+        etFasilitas.setText(yFasilitas);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nama = etNama.getText().toString();
+                alamat = etAlamat.getText().toString();
+                noTelepon = etNoTelepon.getText().toString();
+                fasilitas = etFasilitas.getText().toString();
+
+                if (nama.trim().isEmpty()){
+                    etNama.setError("Nama Harus Diisi");
+                }
+                else if (alamat.trim().isEmpty()){
+                    etAlamat.setError("Alamat Harus Diisi");
+                }
+                else if (noTelepon.trim().isEmpty()){
+                    etNoTelepon.setError("No. Telepon Harus Diisi");
+                }
+                else if (fasilitas.trim().isEmpty()){
+                    etFasilitas.setError("Fasilitas Harus Diisi");
+                }
+                else {
+                    prosesSimpan();
+                }
+            }
+        });
+    }
+    private void prosesSimpan(){
+        APIRequestData API = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Call<ModelResponse> proses = API.ardUpdate(yId, nama, alamat, noTelepon, fasilitas);
+
+        proses.enqueue(new Callback<ModelResponse>() {
+            @Override
+            public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                String kode = response.body().getKode();
+                String pesan = response.body().getPesan();
+
+                Toast.makeText(UbahActivity.this, "Kode : " + kode + " Pesan : " + pesan, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<ModelResponse> call, Throwable t) {
+                Toast.makeText(UbahActivity.this, "Gagal Menghubungi Server!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/si61/internationalschool/Adapter/AdapterSekolah.java
+++ app/src/main/java/com/si61/internationalschool/Adapter/AdapterSekolah.java
@@ -1,18 +1,32 @@
 package com.si61.internationalschool.Adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.si61.internationalschool.API.APIRequestData;
+import com.si61.internationalschool.API.RetroServer;
+import com.si61.internationalschool.Activity.MainActivity;
+import com.si61.internationalschool.Activity.TambahActivity;
+import com.si61.internationalschool.Activity.UbahActivity;
+import com.si61.internationalschool.Model.ModelResponse;
 import com.si61.internationalschool.Model.ModelSekolah;
 import com.si61.internationalschool.R;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdapterSekolah extends RecyclerView.Adapter<AdapterSekolah.VHSekolah>{
     private Context ctx;
@@ -58,6 +72,58 @@ public class AdapterSekolah extends RecyclerView.Adapter<AdapterSekolah.VHSekola
             tvAlamat = itemView.findViewById(R.id.tv_alamat);
             tvNoTelp = itemView.findViewById(R.id.tv_noTelepon);
             tvFasilitas = itemView.findViewById(R.id.tv_fasilitas);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder pesan = new AlertDialog.Builder(ctx);
+                    pesan.setTitle("Perhatian");
+                    pesan.setMessage("Anda memilih "+ tvNama.getText().toString() +", Operasi Apa yang Akan Dilakukan ?");
+
+                    pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent kirim = new Intent(ctx, UbahActivity.class);
+                            kirim.putExtra("xId", tvId.getText().toString());
+                            kirim.putExtra("xNama", tvNama.getText().toString());
+                            kirim.putExtra("xAlamat", tvAlamat.getText().toString());
+                            kirim.putExtra("xNoTelp", tvNoTelp.getText().toString());
+                            kirim.putExtra("xFasilitas", tvFasilitas.getText().toString());
+                            ctx.startActivity(kirim);
+                        }
+                    });
+                    pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            prosesHapus(tvId.getText().toString());
+
+                        }
+                    });
+                    pesan.show();
+                    return false;
+                }
+            });
+        }
+        void prosesHapus(String id){
+            APIRequestData API = RetroServer.konekRetrofit().create(APIRequestData.class);
+            Call<ModelResponse> proses = API.ardDelete(id);
+
+            proses.enqueue(new Callback<ModelResponse>() {
+                @Override
+                public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                    String kode = response.body().getKode();
+                    String pesan = response.body().getPesan();
+
+                    Toast.makeText(ctx, "Kode : " + kode + " Pesan : " + pesan, Toast.LENGTH_SHORT).show();
+                    ((MainActivity) ctx).retrieveSekolah();
+                }
+
+                @Override
+                public void onFailure(Call<ModelResponse> call, Throwable t) {
+                    Toast.makeText(ctx,"Gagal Menghubungi Server: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
         }
     }
 }
